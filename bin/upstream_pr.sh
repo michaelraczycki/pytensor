17b9d618621ebaf6d056baba783b1e771d154175
Thomas Wiecki <thomas.wiecki@gmail.com>
2022-11-30 18:11:22 +0100

    Rename branch. Remove auto-pushing which gh already does.

--- bin/upstream_pr.sh
+++ bin/upstream_pr.sh
@@ -11,7 +11,7 @@ set -e
 
 git checkout main
 git pull origin main
-git checkout -b downstream_$1
+git checkout -b upstream_$1
 
 echo "Downloading patch..."
 wget -O $1.patch https://patch-diff.githubusercontent.com/raw/pymc-devs/pytensor/pull/$1.patch
@@ -36,8 +36,6 @@ fi
 
 echo "Running pre-commit"
 pre-commit run --all
-
-git push origin downstream_$1
 
 title=$(curl https://api.github.com/repos/pymc-devs/pytensor/pulls/$1 2>/dev/null | jq '.title')
 gh pr create --repo aesara-devs/aesara --title "Upstreaming PyTensor PR $1: $title" --body "Upstreaming https://github.com/pymc-devs/pytensor/pull/$1. PR port done by upstream_pr.sh script."
